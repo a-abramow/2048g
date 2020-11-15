160a5643ce3551fd33a22411f77d2fc2179a44ea
Andrey <abramow0000@yandex.ru>
2020-11-15 05:50:20 +0300

    GU interface

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from logics import *
+import pygame
+import sys
 
+def draw_interface():
+    pygame.draw.rect(screen, WHITE, TITLE_REC)
+    font = pygame.font.SysFont("stxingkai", 70)
+    pretty_print(mas)
+    for row in range(BLOCKS):
+        for column in range(BLOCKS):
+            value = mas[row][column]
+            text = font.render(f'{value}', True, BLACK)
+            w = column * SIZE_BLOCK + (column + 1) * MARGIN
+            h = row * SIZE_BLOCK + (row + 1) * MARGIN + SIZE_BLOCK
+            pygame.draw.rect(screen, COLORS[value], (w, h, SIZE_BLOCK, SIZE_BLOCK))
+            if value != 0:
+                font_w, font_h = text.get_size()
+                text_x = w + (SIZE_BLOCK - font_w) / 2
+                text_y = h + (SIZE_BLOCK - font_h) / 2
+                screen.blit(text, (text_x, text_y))
 
 mas = [
     [0, 0, 0, 0],
@@ -8,25 +26,44 @@ mas = [
     [0, 0, 0, 0],
 ]
 
+COLORS = {
+    0: (130, 130, 130),
+    2: (255, 255, 255),
+    4: (255, 255, 128),
+    8: (255, 255, 0),
+}
+WHITE = (255, 255, 255)
+GRAY = (130, 130, 130)
+BLACK = (0, 0, 0)
+
+BLOCKS = 4
+SIZE_BLOCK = 110
+MARGIN = 10
+WIDTH = BLOCKS * SIZE_BLOCK + (BLOCKS + 1) * MARGIN
+HEIGHT = WIDTH + 110
 mas[1][2] = 2
 mas[3][0] = 4
 print(get_empty_list(mas))
 pretty_print(mas)
 
-# начать цикл игры:
-# ждать от пользователя команды
-# когда получится, обработать массив
-# найти пустые клетки
-# если есть пустые клетки, выбрать одну случайно
-# и положить туда 2 либо 4
-# если пустых нети двигать нельзя, игра окончена
-while is_zero_in_mas(mas):
-    input()
-    empty = get_empty_list(mas)
-    random.shuffle(empty)
-    random_num = empty.pop()
-    x, y = get_index_from_number(random_num)
-    mas = insert_2_or_4(mas, x, y)
-    print(f'Мы заполнили элемент под номером {random_num}')
-    pretty_print(mas)
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("2048")
+TITLE_REC = pygame.Rect(0, 0, WIDTH, 110)
+draw_interface()
+pygame.display.update()
 
+while is_zero_in_mas(mas):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN:
+            empty = get_empty_list(mas)
+            random.shuffle(empty)
+            random_num = empty.pop()
+            x, y = get_index_from_number(random_num)
+            mas = insert_2_or_4(mas, x, y)
+            print(f'Мы заполнили элемент под номером {random_num}')
+            draw_interface()
+            pygame.display.update()
