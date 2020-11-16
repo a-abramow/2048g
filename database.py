fbdb1fdeac20b9937f13bc37d161d7a06fc05185
Andrey <abramow0000@yandex.ru>
2020-11-16 07:33:20 +0300

    put tries on top

--- database.py
+++ database.py
@@ -4,19 +4,24 @@ bd = sqlite3.connect("2040.sqlite")
 
 cur = bd.cursor()
 
+# создаем таблицу с именем RECORDS и полями name и score
 cur.execute("""
-create table if not exists RECORDS (
+create table if not exists RECORDS (  
     name text,
     score integer
 )""")
 
-cur.execute("""
-SELECT name gamer, max(score) score FROM RECORDS
-GROUP by name
-ORDER by score DESC
-limit 3
-""")
 
-result = cur.fetchall()
-print(result)
-cur.close()
+# запрос с выбором и сортировкой трех лучших игроков
+def get_best():          # создаем функцию с запросом для использовании в main.py
+    cur.execute("""
+    SELECT name gamer, max(score) score FROM RECORDS
+    GROUP by name
+    ORDER by score DESC
+    limit 3
+    """)
+    return cur.fetchall()
+
+
+print(get_best())            #
+
